9e9d3b599d28dc1f86851b9f1e6f3131be7904ad
Executor <just4dotaalex@gmail.com>
2023-11-12 19:14:13 +0200

    feat: added cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { log } from './common/helpers/winston.logger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger({ instance: log }),
+    cors: true,
   });
   await app.listen(process.env.PORT);
 }
