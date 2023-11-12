29e11bb2006e25906cdcaa12185d0addf45f1d83
Executor <just4dotaalex@gmail.com>
2023-11-12 22:57:15 +0200

    feat: added user type

---
+++ src/types/user.type.ts
@@ -0,0 +1,10 @@
+export type User = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  sex: string;
+  course: number;
+  direction: string;
+  created_at: Date;
+};
