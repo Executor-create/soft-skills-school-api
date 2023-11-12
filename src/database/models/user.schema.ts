7a5b1bf969ba4e54601c15d61d34284498771e29
Executor <just4dotaalex@gmail.com>
2023-11-12 22:56:26 +0200

    feat: added user schema

---
+++ src/database/models/user.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  firstName: string;
+
+  @Prop()
+  lastName: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  sex: string;
+
+  @Prop()
+  course: number;
+
+  @Prop()
+  direction: string;
+
+  @Prop()
+  created_at: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
