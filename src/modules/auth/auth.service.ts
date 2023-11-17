e15be90379585e6bc8254e91ab3bcc16d0a14357
Executor <just4dotaalex@gmail.com>
2023-11-17 20:34:16 +0200

    feat: added auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User as UserDB } from 'src/database/models/user.schema';
+import { User as UserDB, UserDocument } from 'src/database/models/user.schema';
 import { User } from 'src/types/user.type';
-import { SignUpDto } from './dto/auth.dto';
+import { SignInDto, SignUpDto } from './dto/auth.dto';
 import { LoggerService } from 'src/common/helpers/winston.logger';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(UserDB.name)
     private readonly userModel: Model<UserDB>,
+    private readonly jwtService: JwtService,
     private readonly logger: LoggerService,
   ) {}
 
@@ -29,5 +31,59 @@ export class AuthService {
     this.logger.info('Created user:', newUser);
 
     return newUser;
+  }
+
+  async signIn(signInDto: SignInDto): Promise<User> {
+    const { email, password } = signInDto;
+
+    const fetchedUser = await this.userModel.findOne({ email });
+
+    if (!fetchedUser) {
+      this.logger.error('User not found');
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+    }
+
+    if (email == fetchedUser.email) {
+      throw new HttpException(
+        'This email is already registered',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    this.logger.info('Found user:', fetchedUser);
+
+    const comparedPassword = await bcrypt.compare(
+      password,
+      fetchedUser.password,
+    );
+
+    if (!comparedPassword) {
+      this.logger.error('Incorrect email or password');
+      throw new HttpException(
+        'Incorrect email or password',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const updatedUser = await this.addJwtToken(fetchedUser);
+
+    return updatedUser;
+  }
+
+  async addJwtToken(user: UserDocument): Promise<User> {
+    const { _id, email } = user;
+    const payload = { sub: _id, email: email };
+
+    const token = await this.jwtService.signAsync(payload);
+
+    const updatedUser = await this.userModel.findByIdAndUpdate(
+      { _id },
+      { $set: { token: token } },
+      { new: true },
+    );
+
+    this.logger.info('Updated user:', updatedUser);
+
+    return updatedUser;
   }
 }
