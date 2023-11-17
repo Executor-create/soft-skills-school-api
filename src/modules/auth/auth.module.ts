565d4dbb4501b7d76643e0a565f783c1ce7ee3fa
Executor <just4dotaalex@gmail.com>
2023-11-17 20:33:57 +0200

    feat: added auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,10 +4,18 @@ import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/database/models/user.schema';
 import { LoggerService } from 'src/common/helpers/winston.logger';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
   ],
   providers: [AuthService, LoggerService],
   controllers: [AuthController],
