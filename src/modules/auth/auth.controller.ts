abcf219873452750c88e86ca5956d6275bc81f27
Executor <just4dotaalex@gmail.com>
2023-11-17 20:34:34 +0200

    feat: added auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { User } from 'src/types/user.type';
-import { SignUpDto } from './dto/auth.dto';
+import { SignInDto, SignUpDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -11,6 +11,14 @@ export class AuthController {
   @HttpCode(201)
   async signUp(@Body() signUpDto: SignUpDto): Promise<User> {
     const user = await this.authService.signUp(signUpDto);
+
+    return user;
+  }
+
+  @Post('/signin')
+  @HttpCode(200)
+  async signIn(@Body() signInDto: SignInDto): Promise<User> {
+    const user = await this.authService.signIn(signInDto);
 
     return user;
   }
