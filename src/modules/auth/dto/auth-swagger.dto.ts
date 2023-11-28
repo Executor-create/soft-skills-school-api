89f7f43ce42f07221e88e29a2d813b68de47fbf4
Executor <just4dotaalex@gmail.com>
2023-11-28 02:43:19 +0200

    feat: added swagger with responses

---
+++ src/modules/auth/dto/auth-swagger.dto.ts
@@ -0,0 +1,14 @@
+import { AuthDto } from './auth.dto';
+import { OmitType, PickType } from '@nestjs/swagger';
+
+export class SignUpRequest extends OmitType(AuthDto, [
+  'token',
+  'created_at',
+] as const) {}
+
+export class SignUpResponse extends OmitType(AuthDto, ['token'] as const) {}
+
+export class SignInRequest extends PickType(AuthDto, [
+  'email',
+  'password',
+] as const) {}
