a296008666240991608f897d3e599ffbd7cf359e
Executor <just4dotaalex@gmail.com>
2023-12-05 18:41:57 +0200

    feat: added user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL),
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService, Logger],
