7ecb7bd551cdf11524d993bb3f39c634e8d29c99
Executor-create <just4dotaalex@gmail.com>
2023-12-18 17:44:49 +0200

    feat: added get question

--- src/modules/question/question.controller.ts
+++ src/modules/question/question.controller.ts
@@ -1,18 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { Question } from 'src/types/question.type';
 import { QuestionService } from './question.service';
 import { CreateQuestionDto } from './dto/question.dto';
+import { findByIdDto } from 'src/common/dto/findById.dto';
 
 @Controller('questions')
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
 
   @Post()
+  @HttpCode(201)
   async createQuestion(
     @Body() createQuestionDto: CreateQuestionDto,
   ): Promise<Question> {
     const question = await this.questionService.create(createQuestionDto);
 
     return question;
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  async getQuestion(@Param() id: findByIdDto): Promise<Question> {
+    const fetchedQuestion = await this.questionService.get(id);
+
+    return fetchedQuestion;
   }
 }
