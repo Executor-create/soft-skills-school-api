5dfe846c566fbcb150adf46960c109a9e6afab7d
Executor-create <just4dotaalex@gmail.com>
2023-12-18 18:33:40 +0200

    feat: added user dto

---
+++ src/modules/user/dto/user.dto.ts
@@ -0,0 +1,66 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsNumber, IsEmail } from 'class-validator';
+
+export class UserDto {
+  @ApiProperty({
+    example: 'John',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @ApiProperty({
+    example: 'Smith',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @ApiProperty({
+    example: 'johnsmith@gmail.com',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    example: 'qwerty123',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @ApiProperty({
+    example: 'Male|Female',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  sex: string;
+
+  @ApiProperty({
+    example: 3,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  course: number;
+
+  @ApiProperty({
+    example: 'Design',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  direction: string;
+
+  @ApiProperty()
+  created_at: Date;
+
+  @ApiProperty()
+  token: string;
+}
