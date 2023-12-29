10ca1ec3ab797afbd5db26b9ae1a9f39dea2be2e
Executor-create <just4dotaalex@gmail.com>
2023-12-30 01:45:38 +0200

    refactoring: refactored update question dto for swagger

--- src/modules/question/dto/question-swagger.dto.ts
+++ src/modules/question/dto/question-swagger.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty, OmitType } from '@nestjs/swagger';
+import { ApiProperty, OmitType, PickType } from '@nestjs/swagger';
 import { QuestionDto } from './question.dto';
 
 export class CreateQuestionRequest extends OmitType(QuestionDto, [
@@ -12,23 +12,19 @@ export class GetQuestionResponse extends QuestionDto {}
 
 export class DeleteQuestionResponse extends QuestionDto {}
 
-export class UpdateQuestionRequest extends OmitType(QuestionDto, [
-  '_id',
-  'created_at',
+export class UpdateQuestionRequest extends PickType(QuestionDto, [
+  'question',
 ] as const) {
   @ApiProperty({
-    example: 'Do you have good soft skills',
+    example: 'Do you have good soft skills?',
     required: true,
   })
   question: string;
 }
 
-export class UpdateQuestionResponse extends OmitType(QuestionDto, [
-  '_id',
-  'created_at',
-] as const) {
+export class UpdateQuestionResponse extends QuestionDto {
   @ApiProperty({
-    example: 'Do you have good soft skills',
+    example: 'Do you have good soft skills?',
     required: true,
   })
   question: string;
