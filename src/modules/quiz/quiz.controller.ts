7f225ffc9150509bbb59668724602b4d68b6d4f8
Executor-create <just4dotaalex@gmail.com>
2024-01-09 18:54:19 +0200

    feat: added endpoints for crud

---
+++ src/modules/quiz/quiz.controller.ts
@@ -0,0 +1,136 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { TestService } from './quiz.service';
+import { Test } from 'src/types/test.type';
+import { CreateTestDto } from './dto/quiz.dto';
+import { findByIdDto } from 'src/common/dto/findById.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from 'src/common/enums/user-role.enum';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { RolesGuard } from 'src/common/guards/role.guard';
+import {
+  CreateTestRequest,
+  CreateTestResponse,
+  DeleteTestResponse,
+  GetAllTestsResponse,
+  GetTestResponse,
+} from './dto/quiz-swagger.dto';
+import { deleteByIdDto } from 'src/common/dto/deleteById.dto';
+import { isValidObjectId } from 'mongoose';
+
+@ApiTags('Test')
+@Controller('tests')
+export class TestController {
+  constructor(private readonly testService: TestService) {}
+
+  @Post()
+  @HttpCode(200)
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  @ApiOperation({ summary: 'Add test' })
+  @ApiBearerAuth()
+  @ApiBody({
+    type: CreateTestRequest,
+    description: 'JSON structure for test',
+  })
+  @ApiResponse({
+    type: CreateTestResponse,
+    status: 200,
+    description: 'The test has been successfully created',
+  })
+  async createTest(@Body() createTestDto: CreateTestDto): Promise<Test> {
+    const newTest = await this.testService.create(createTestDto);
+
+    return newTest;
+  }
+
+  @Get()
+  @HttpCode(201)
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  @ApiOperation({ summary: 'Get all tests' })
+  @ApiBearerAuth()
+  @ApiResponse({
+    type: [GetAllTestsResponse],
+    status: 201,
+    description: 'The tests successfully retrieve from database',
+  })
+  async getTests(): Promise<Test[]> {
+    const fetchedTests = await this.testService.getAll();
+
+    return fetchedTests;
+  }
+
+  @Get(':id')
+  @HttpCode(201)
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  @ApiOperation({ summary: 'Get test' })
+  @ApiBearerAuth()
+  @ApiResponse({
+    type: GetTestResponse,
+    status: 201,
+    description: 'The test successfully retrieve from database',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Test not found',
+  })
+  async getTest(@Param() testId: findByIdDto): Promise<Test> {
+    if (!isValidObjectId(testId)) {
+      throw new HttpException(
+        'Bad ObjectId format in the path',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const fetchedTest = await this.testService.get(testId);
+
+    return fetchedTest;
+  }
+
+  @Delete(':id')
+  @HttpCode(201)
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  @ApiOperation({ summary: 'Delete test' })
+  @ApiBearerAuth()
+  @ApiResponse({
+    type: DeleteTestResponse,
+    status: 201,
+    description: 'The test successfully deleted from database',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Test not found',
+  })
+  async deleteTest(@Param() testId: deleteByIdDto): Promise<Test> {
+    if (!isValidObjectId(testId)) {
+      throw new HttpException(
+        'Bad ObjectId format in the path',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const deletedTest = await this.testService.delete(testId);
+
+    return deletedTest;
+  }
+}
