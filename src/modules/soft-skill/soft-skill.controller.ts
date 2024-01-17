12dd775c56d51493b25d9f5ce00810a82588b177
Executor-create <just4dotaalex@gmail.com>
2024-01-17 05:18:04 +0200

    feat: added swagger documentation for create soft skill endpoint

--- src/modules/soft-skill/soft-skill.controller.ts
+++ src/modules/soft-skill/soft-skill.controller.ts
@@ -1,9 +1,31 @@
-import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { SoftSkillService } from './soft-skill.service';
 import { CreateSoftSkillDto } from './dto/soft-skill.dto';
 import { SoftSkill } from 'src/types/soft-skill.type';
 import { findByIdDto } from 'src/common/dto/findById.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from 'src/common/enums/user-role.enum';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { RolesGuard } from 'src/common/guards/role.guard';
+import {
+  CreateSoftSkillDtoRequest,
+  CreateSoftSkillDtoResponse,
+} from './dto/soft-skill-swagger.dto';
 
 @ApiTags('Soft Skill')
 @Controller('soft-skills')
@@ -12,6 +34,19 @@ export class SoftSkillController {
 
   @Post()
   @HttpCode(201)
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  @ApiOperation({ summary: 'Add soft skill' })
+  @ApiBearerAuth()
+  @ApiBody({
+    type: CreateSoftSkillDtoRequest,
+    description: 'JSON structure for soft skill',
+  })
+  @ApiResponse({
+    type: CreateSoftSkillDtoResponse,
+    status: 201,
+    description: 'The soft skill has been successfully created',
+  })
   async createSoftSkill(
     @Body() createSoftSkillDto: CreateSoftSkillDto,
   ): Promise<SoftSkill> {
