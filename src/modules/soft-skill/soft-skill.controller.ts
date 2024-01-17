1a5436724c3389e518e651541fc3b6cfd029cb29
Executor-create <just4dotaalex@gmail.com>
2024-01-17 04:43:58 +0200

    feat: added create soft skill endpoint

--- src/modules/soft-skill/soft-skill.controller.ts
+++ src/modules/soft-skill/soft-skill.controller.ts
@@ -1,6 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { SoftSkillService } from './soft-skill.service';
+import { CreateSoftSkillDto } from './dto/soft-skill.dto';
+import { SoftSkill } from 'src/types/soft-skill.type';
 
 @ApiTags('Soft Skill')
-@Controller('soft-skill')
-export class SoftSkillController {}
+@Controller('soft-skills')
+export class SoftSkillController {
+  constructor(private readonly softSkillService: SoftSkillService) {}
+
+  @Post()
+  async createSoftSkill(
+    @Body() createSoftSkillDto: CreateSoftSkillDto,
+  ): Promise<SoftSkill> {
+    const newSoftSkill = await this.softSkillService.create(createSoftSkillDto);
+
+    return newSoftSkill;
+  }
+}
--- src/modules/soft-skill/soft-skill.service.ts
+++ src/modules/soft-skill/soft-skill.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { SoftSkill as SoftSkillDB } from 'src/database/models/soft-skill.schema';
+import { CreateSoftSkillDto } from './dto/soft-skill.dto';
+import { SoftSkill } from 'src/types/soft-skill.type';
 
 @Injectable()
-export class SoftSkillService {}
+export class SoftSkillService {
+  constructor(
+    @InjectModel(SoftSkillDB.name)
+    private readonly softSkillModel: Model<SoftSkillDB>,
+  ) {}
+
+  async create(createSoftSkillDto: CreateSoftSkillDto): Promise<SoftSkill> {
+    const softSkill = new this.softSkillModel(createSoftSkillDto);
+    softSkill.created_at = new Date();
+
+    const newSoftSkill = await softSkill.save();
+
+    return newSoftSkill;
+  }
+}
