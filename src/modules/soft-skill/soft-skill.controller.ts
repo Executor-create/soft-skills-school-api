31374535ab5a92231ed613418264a3185ab0acbc
Executor-create <just4dotaalex@gmail.com>
2024-01-17 05:04:39 +0200

    feat: add get method for fetching soft skills

--- src/modules/soft-skill/soft-skill.controller.ts
+++ src/modules/soft-skill/soft-skill.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { SoftSkillService } from './soft-skill.service';
 import { CreateSoftSkillDto } from './dto/soft-skill.dto';
 import { SoftSkill } from 'src/types/soft-skill.type';
+import { findByIdDto } from 'src/common/dto/findById.dto';
 
 @ApiTags('Soft Skill')
 @Controller('soft-skills')
@@ -10,11 +11,20 @@ export class SoftSkillController {
   constructor(private readonly softSkillService: SoftSkillService) {}
 
   @Post()
+  @HttpCode(201)
   async createSoftSkill(
     @Body() createSoftSkillDto: CreateSoftSkillDto,
   ): Promise<SoftSkill> {
     const newSoftSkill = await this.softSkillService.create(createSoftSkillDto);
 
     return newSoftSkill;
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  async getSoftSkill(@Param() softSkillId: findByIdDto): Promise<SoftSkill> {
+    const fetchedSoftSkill = await this.softSkillService.get(softSkillId);
+
+    return fetchedSoftSkill;
   }
 }
--- src/modules/soft-skill/soft-skill.service.ts
+++ src/modules/soft-skill/soft-skill.service.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { SoftSkill as SoftSkillDB } from 'src/database/models/soft-skill.schema';
 import { CreateSoftSkillDto } from './dto/soft-skill.dto';
 import { SoftSkill } from 'src/types/soft-skill.type';
+import { findByIdDto } from 'src/common/dto/findById.dto';
+import { LoggerService } from 'src/common/helpers/winston.logger';
 
 @Injectable()
 export class SoftSkillService {
   constructor(
     @InjectModel(SoftSkillDB.name)
     private readonly softSkillModel: Model<SoftSkillDB>,
+    private readonly logger: LoggerService,
   ) {}
 
   async create(createSoftSkillDto: CreateSoftSkillDto): Promise<SoftSkill> {
@@ -19,5 +22,20 @@ export class SoftSkillService {
     const newSoftSkill = await softSkill.save();
 
     return newSoftSkill;
+  }
+
+  async get(softSkillId: findByIdDto): Promise<SoftSkill> {
+    const { id } = softSkillId;
+
+    const fetchedSoftSkill = await this.softSkillModel.findById(id);
+
+    this.logger.info('Fetched soft skill:', fetchedSoftSkill);
+
+    if (!fetchedSoftSkill) {
+      this.logger.error('Soft skill not found');
+      throw new HttpException('Soft skill not found', HttpStatus.NOT_FOUND);
+    }
+
+    return fetchedSoftSkill;
   }
 }
