2382a1a6279ef67b02a2ebe4d0a9b409df034b20
Executor-create <just4dotaalex@gmail.com>
2024-01-17 04:43:33 +0200

    feat: added create soft skill dto

---
+++ src/modules/soft-skill/dto/soft-skill.dto.ts
@@ -0,0 +1,24 @@
+import { PickType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { ObjectId } from 'mongoose';
+
+export class SoftSkillDto {
+  @ApiProperty({
+    example: '659c6ea61266eb9da8f600fc',
+    required: true,
+  })
+  _id: ObjectId;
+
+  @ApiProperty({
+    example: 'Communication',
+    required: true,
+  })
+  type: string;
+
+  @ApiProperty()
+  created_at: Date;
+}
+
+export class CreateSoftSkillDto extends PickType(SoftSkillDto, [
+  'type',
+] as const) {}
