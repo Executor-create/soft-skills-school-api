305e5d5e45f4781b12fc7f4ab7828e7bdac84cb7
Executor-create <just4dotaalex@gmail.com>
2024-01-17 04:02:50 +0200

    feat: import soft skill schema

--- src/modules/soft-skill/soft-skill.module.ts
+++ src/modules/soft-skill/soft-skill.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { SoftSkillService } from './soft-skill.service';
 import { SoftSkillController } from './soft-skill.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  SoftSkill,
+  SoftSkillSchema,
+} from 'src/database/models/soft-skill.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: SoftSkill.name, schema: SoftSkillSchema },
+    ]),
+  ],
   providers: [SoftSkillService],
   controllers: [SoftSkillController],
 })
