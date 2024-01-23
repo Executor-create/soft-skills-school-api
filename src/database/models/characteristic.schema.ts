e8e5fdc44ec93dbc279655a0f17bac08a34325d7
Executor-create <just4dotaalex@gmail.com>
2024-01-23 02:09:26 +0200

    feat: added characteristic schema

---
+++ src/database/models/characteristic.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument, ObjectId } from 'mongoose';
+
+export type CharacteristicDocument = HydratedDocument<Characteristic>;
+
+@Schema()
+export class Characteristic {
+  @Prop()
+  titles: string[];
+
+  @Prop({ type: mongoose.Types.ObjectId, ref: 'softSkill' })
+  softSkill: {
+    softSkillId: ObjectId;
+    type: string;
+    created_at: Date;
+  };
+
+  @Prop()
+  created_at: Date;
+}
+
+export const CharacteristicSchema =
+  SchemaFactory.createForClass(Characteristic);
