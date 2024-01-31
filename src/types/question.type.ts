a80ee457ff6774518be78c178d4a174131481093
Executor-create <just4dotaalex@gmail.com>
2024-01-31 06:08:40 +0200

    feat: change question type

--- src/types/question.type.ts
+++ src/types/question.type.ts
@@ -1,7 +1,21 @@
+import { ObjectId, Types } from 'mongoose';
+
+export type CharacteristicWithSoftSkill = {
+  characteristicId: Types.ObjectId;
+  title: string;
+  softSkill: {
+    softSkillId: ObjectId;
+    type: string;
+  };
+  created_at: Date;
+};
+
 export type Question = {
   question: string;
   type: string;
-  category: string;
+  answers?: string[];
+  correctAnswers?: boolean[];
+  characteristics: CharacteristicWithSoftSkill[];
   points: number;
   created_at: Date;
 };
